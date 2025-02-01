cc0e7f517743d175bc2a20af4a4371f714e1f304
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-02-01 14:35:37 -0600

    The Dominion King

---
+++ Dominion_of_kings.cs
@@ -0,0 +1,47 @@
+﻿//Shrijan Paudel
+//01/25/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Dominion_of_Kings
+{
+    public class Dominion_of_kings
+    {
+
+        // declaring a private variable;
+        private int provinces=-1;  
+        private int duchies=-1;
+        private int estates = -1;
+
+
+        //Getters and setters
+        public int Provinces
+        {
+            get { return this.provinces; }
+            set { this.provinces = value;}
+        }
+        public int Duchies
+        {
+            get { return this.duchies; }
+            set { this.duchies = value; }
+        }
+        public int Estates
+        {
+            get { return this.estates; }
+            set { this.estates = value; }
+        }
+
+        //method to calculate total point 
+        public int Calculate()
+        {
+            int total_points = (Provinces * 6) + (Duchies * 3) + (Estates * 1);
+            return total_points;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿//Written by Shrijan Paudel
+//01/27/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Dominion_of_Kings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dominion_of_kings dominion declares a variable named dominion of type Dominion_of_kings
+            //Dominion_of_kings is likely a class or struct defined elsewhere in your code.
+
+            Dominion_of_kings dominion = new Dominion_of_kings();
+
+            Console.WriteLine("Enter the number of Provinces");    // User Input
+            int provinces_num = Convert.ToInt32(Console.ReadLine()); // direct convert to integer
+
+            Console.WriteLine("Enter the number of duchies");
+            int duchies_num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of estates");
+            int estates_num = Convert.ToInt32(Console.ReadLine());
+
+            //assigning a value to new domainion variable
+
+            dominion.Provinces = provinces_num;
+            dominion.Duchies = duchies_num;
+            dominion.Estates = estates_num;
+
+
+           // CallingConvention a method
+            int total = dominion.Calculate();
+
+
+
+            if (total >= 0)
+            {
+                Console.WriteLine($"Total point of the kingdom is {total}");
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUTS");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
